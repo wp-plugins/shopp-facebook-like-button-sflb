79a5a73757a13ddeae9afbf5d4d97e86ecdc0c5a
Shoppdeveloper.com <Shoppdeveloper.com@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-03-19 18:14:16 +0000

    Updating 1.3.1 fies
    git-svn-id: https://plugins.svn.wordpress.org/shopp-facebook-like-button-sflb/trunk@1116224 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/other_plugins.php
+++ includes/other_plugins.php
@@ -1,24 +1,29 @@
-<?php
-        echo "<tr>";
-		echo "<th scope='row' valign='top'><label for='plugins'>".__( 'Plugins developed so far:','sflb')."</label></th>";
-		echo "<td>";
-		echo "<p><a href='http://www.shoppdeveloper.com/downloads/shopp-images-to-variations/' title='".__('Shopp Variations to Images','sflb')."'>Shopp Variations to Images</a></p>";
-		echo "<p>".__('This plugin will enhance your customer\'s shop experience by adjusting the product image to the variation of their choice', 'sflb').".</p>";
-		echo "<p>".__('For example, when selecting a different print, or color, or model, the product image would adjust to show that print, or color, or model', 'sflb').".</p>";
-		echo "<BR />";
-		echo "<p><a href='http://www.shoppdeveloper.com/downloads/shopp-bulk-price-editor/' title='".__('Shopp Bulk Price Editor','sflb')."'>Shopp Bulk Price Editor</a></p>";
-		echo "<p>".__('Change the price of multiple products at once', 'sflb').".</p>";
-		echo "<p>".__('For example, increase all product prices in a certain category with a certain amount, or percentage', 'sflb').".</p>";
-		echo "<BR />";
-		echo "<p><a href='http://www.shoppdeveloper.com/downloads/shopp-bulk-product-importer/' title='".__('Shopp Bulk Product Importer','sflb')."'>Shopp Bulk Product Importer</a></p>";
-		echo "<p>".__('Import products from a CSV file', 'sflb').".</p>";
-		echo "<BR />";
-		echo "<p><a href='http://www.shoppdeveloper.com/downloads/shopp-cleaner/' title='".__('Shopp Cleaner','sflb')."'>Shopp Cleaner</a></p>";
-		echo "<p>".__('Cleanup data left behind by deleted products. Or start from scratch, remove all products, categories, tags but leave orders and customer data untouched.', 'sflb').".</p>";
-		echo "<BR />";
-		echo "<p><a href='http://www.shoppdeveloper.com/downloads/shopp-payment-add-on-mollie/' title='".__('Shopp Payment Add-on Mollie','sflb')."'>Shopp Payment Add-on Mollie</a></p>";
-		echo "<p>".__('For Dutch merchants. Add the features of the new Mollie API to your shop', 'sflb').".</p>";
-		echo "<p>".__('Accept Bancontact/Mister Cash, Bitcoin, Creditcard, iDEAL, Banktransfer, PayPal, paysafecard, and Sofort Banking payments', 'sflb').".</p>";
-		echo "</td>";
-		echo "</tr>";
-?>
+<tr>
+	<th scope='row' valign='top'><label for='plugins'><?php _e( 'Plugins developed so far:','sflb'); ?></label></th>
+	<td>
+		<p><a href='http://www.shoppdeveloper.com/downloads/shopp-images-to-variations/' title='<?php _e('Shopp Variations to Images','sflb'); ?>'>Shopp Variations to Images</a></p>
+		<p><?php _e('This plugin will enhance your customer\'s shop experience by adjusting the product image to the variation of their choice', 'sflb'); ?>.</p>
+		<p><?php _e('For example, when selecting a different print, or color, or model, the product image would adjust to show that print, or color, or model', 'sflb'); ?>.</p>
+		<BR />
+		<p><a href='http://www.shoppdeveloper.com/downloads/shopp-bulk-price-editor/' title='<?php _e('Shopp Bulk Price Editor','sflb'); ?>'>Shopp Bulk Price Editor</a></p>
+		<p><?php _e('Change the price of multiple products at once', 'sflb'); ?>.</p>
+		<p><?php _e('For example, increase all product prices in a certain category with a certain amount, or percentage', 'sflb'); ?>.</p>
+		<BR />
+		<p><a href='http://www.shoppdeveloper.com/downloads/shopp-bulk-product-importer/' title='<?php _e('Shopp Bulk Product Importer','sflb'); ?>'>Shopp Bulk Product Importer</a></p>
+		<p><?php _e('Import products from a CSV file', 'sflb'); ?>.</p>
+		<BR />
+		<p><a href='http://www.shoppdeveloper.com/downloads/shopp-product-exporter/' title='<?php _e('Shopp Bulk Product Exporter','sflb'); ?>'>Shopp Bulk Product Exporter</a></p>
+		<p><?php _e('Export all product details to a CSV file', 'sflb'); ?>.</p>
+		<BR />
+		<p><a href='http://www.shoppdeveloper.com/downloads/shopp-cleaner/' title='<?php _e('Shopp Cleaner','sflb'); ?>.'>Shopp Cleaner</a></p>
+		<p><?php _e('Cleanup data left behind by deleted products. Or start from scratch, remove all products, categories, tags but leave orders and customer data untouched.', 'sflb'); ?></p>
+		<BR />
+		<p><a href='http://www.shoppdeveloper.com/downloads/shopp-payment-add-on-mollie/' title='<?php _e('Shopp Payment Add-on Mollie','sflb'); ?>.'>Shopp Payment Add-on Mollie</a></p>
+		<p><?php _e('For Dutch merchants. Add the features of the new Mollie API to your shop', 'sflb'); ?>.</p>
+		<p><?php _e('Accept Bancontact/Mister Cash, Bitcoin, Creditcard, iDEAL, Banktransfer, PayPal, paysafecard, and Sofort Banking payments', 'sflb'); ?>.</p>
+		<BR />
+		<p><a href='http://www.shoppdeveloper.com/shopp-product-page-browser-plugin/' title='<?php _e('Shopp Product Page Browser','sflb'); ?>.'>Shopp Product Page Browser * FREE *</a></p>
+		<p><?php _e('Add the feature to go from one product to the next/previous without having to go back to the category page first', 'sflb'); ?>.</p>
+		<BR />
+	</td>
+</tr>
